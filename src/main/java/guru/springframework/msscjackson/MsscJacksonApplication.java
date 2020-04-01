c87f919f2f87b3b139d1a77ed1097fff6b09ef09
murilo_r <murilo_r@dell.com>
2020-04-01 15:08:08 -0300

    spring foundation project

---
+++ src/main/java/guru/springframework/msscjackson/MsscJacksonApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscjackson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscJacksonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscJacksonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/msscjackson/MsscJacksonApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscjackson;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscJacksonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
